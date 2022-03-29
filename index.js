1ce5998192e07b77a3fa754647a186827bdb3f72
Sadie <sadie@Sadies-MacBook-Pro.local>
2022-03-29 16:44:08 -0700

    commit tree 0375022f8d8009acf9636af0540dea1c5c1cc0bf

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// const multiply= num1; num2; {
+//     let num1 = 2;
+//     let num2 = 31;
+//     console.log(num1 * num2);
+// }
+let num1 = 2; 
+let num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 1000)
+
+let num3 = 24;
+let num4 = 6;
+const mod = num3 / num4;
+
+let max = Math.ceil(Math.max(20))
